7379b639b506ea4bb608078500d17de113cc2648
Oleksii83 <zaaytav@gmail.com>
2021-12-13 20:55:10 +0200

    the modal on hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Modal from './Modal/Modal';
+import React, { useState } from 'react';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Project</h1>
+      <button
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        Modal
+      </button>
+      {showModal && (
+        <Modal
+          onCloseButtonClick={() => {
+            setShowModal(false);
+          }}
+        />
+      )}
     </div>
   );
 }
---
+++ src/Modal/Modal.js
@@ -0,0 +1,67 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import s from './Modal.module.css';
+import PropTypes from 'prop-types';
+
+type ModalProps = {
+  onCloseButtonClick: () => void,
+};
+
+function Modal(props: ModalProps) {
+  const { onCloseButtonClick } = props;
+  return (
+    <div className={s.Overlay}>
+      <div className={s.Modal}>
+        <span className={s.modalClose} onClick={onCloseButtonClick}>
+          &#10005; {/* HTML code for a multiplication sign */}
+        </span>
+        This is my new modal!
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
+
+//-----------------------------------------------------------------------------------------------------------
+
+// const modalRoot = document.querySelector('#modal-root');
+
+// function Modal({ url, onClose }) {
+//   useEffect(() => {
+//     window.addEventListener('keydown', handleKeyDown);
+//     return () => {
+//       window.removeEventListener('keydown', handleKeyDown);
+//     };
+//   });
+
+//   const handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       onClose();
+//     }
+//   };
+
+//   const handleBackdropClick = e => {
+//     console.log('click');
+//     if (e.target === e.currentTarget) {
+//       onClose();
+//     }
+//   };
+//   return createPortal(
+//     <div className={s.Overlay} onClick={handleBackdropClick}>
+//       <div className={s.Modal}>
+//         <button className="">Yes</button>
+//         <button className="">No</button>
+//         {/* <img src={url} alt="" /> */}
+//       </div>
+//     </div>,
+//     modalRoot,
+//   );
+// }
+
+// export default Modal;
+
+// Modal.propTypes = {
+//   url: PropTypes.string.isRequired,
+//   onClose: PropTypes.func.isRequired,
+// };
